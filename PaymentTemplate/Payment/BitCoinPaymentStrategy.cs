1a9ff3c9bfe5bb0a284e6cdc306228e8f9ff515f
lindelani <lindelani.ngwenya>
2024-09-30 23:39:15 +0200

    Add guard clauses

--- PaymentTemplate/Payment/BitCoinPaymentStrategy.cs
+++ PaymentTemplate/Payment/BitCoinPaymentStrategy.cs
@@ -1,9 +1,14 @@
-﻿namespace PaymentTemplate.Payment
+﻿using Ardalis.GuardClauses;
+
+namespace PaymentTemplate.Payment
 {
     public class BitCoinPaymentStrategy : IPaymentStrategy
     {
         public void ProcessPayment(decimal amount)
         {
+            // Guard clause
+            Guard.Against.NegativeOrZero(amount, nameof(amount));
+
             Console.WriteLine($"Processing Bitcoin payment of {amount:C}");
         }
     }
--- PaymentTemplate/Payment/CreditCardPaymentStrategy.cs
+++ PaymentTemplate/Payment/CreditCardPaymentStrategy.cs
@@ -1,9 +1,13 @@
-﻿namespace PaymentTemplate.Payment
+﻿using Ardalis.GuardClauses;
+
+namespace PaymentTemplate.Payment
 {
     public class CreditCardPaymentStrategy : IPaymentStrategy
     {
         public void ProcessPayment(decimal amount)
         {
+            // Guard clause
+            Guard.Against.NegativeOrZero(amount, nameof(amount));
             Console.WriteLine($"Processing credit card payment of {amount:C}");
         }
     }
